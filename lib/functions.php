9fcad6a54f7b0d57043384f58bf29a911b559773
GPintoNJIT <gkp2@njit.edu>
2024-07-06 14:49:11 -0400

    Removed 'You must be logged in to view this page' message

--- lib/functions.php
+++ lib/functions.php
@@ -40,11 +40,11 @@ function is_logged_in($redirect = false, $destination = "login.php")
 {
     $isLoggedIn = isset($_SESSION["user"]);
     if ($redirect && !$isLoggedIn) {
-        flash("You must be logged in to view this page", "warning");
         die(header("Location: $destination"));
     }
     return $isLoggedIn; //se($_SESSION, "user", false, false);
 }
+
 function has_role($role)
 {
     if (is_logged_in() && isset($_SESSION["user"]["roles"])) {
